d4c737399ed3a5ad1d0d3ef10c162e4a20183ff3
fg91 <fabiograetz@googlemail.com>
2018-10-07 23:50:47 +0200

    Added non_deterministic_behaviour.cu

---
+++ notes/lesson_2/non_deterministic_behaviour.cu
@@ -0,0 +1,20 @@
+/*
+   The programmer can not influence the order in which the blocks are run
+   Therefore this program has 16! different possible outputs
+*/
+#include <stdio.h>
+
+#define NUM_BLOCKS 16
+#define BLOCK_WIDTH 1
+
+__global__ void hello() {
+  printf("Hello world! I'm a thread in block %d\n", blockIdx.x);
+}
+
+int main() {
+  // launch kernels
+  hello<<<NUM_BLOCKS, BLOCK_WIDTH>>>();
+  // forche the printf()s to flush
+  cudaDeviceSynchronize();
+  return 0;
+}
